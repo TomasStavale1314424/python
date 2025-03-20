aa16454c976f886cb105d90078cb0fa815bda004
Tomás Stavale <tomstavale@gmail.com>
2025-03-20 17:25:41 -0300

    PASO1

--- questions.py
+++ questions.py
@@ -1,5 +1,8 @@
 import random
+import sys
+
 # Preguntas para el juego
+
 questions = [
  "¿Qué función se usa para obtener la longitud de una cadena en Python?",
  "¿Cuál de las siguientes opciones es un número entero en Python?",
@@ -28,23 +31,50 @@ answers = [
 correct_answers_index = [1, 2, 0, 3, 1]
 
 # El usuario deberá contestar 3 preguntas
+
 for _ in range(3):
     # Se selecciona una pregunta aleatoria
+
     question_index = random.randint(0, len(questions)-1)
+
     # Se muestra la pregunta y las respuestas posibles
     print(questions[question_index])
     for i, answer in enumerate(answers[question_index]):
         print(f"{i + 1}. {answer}")
+
     # El usuario tiene 2 intentos para responder correctamente
+
     for intento in range(2):
-        user_answer = int(input("Respuesta: ")) - 1
+        """MODIFICO EL INPUT PARA PODER INGRESAR STRING Y VERIFICO QUE NO LO SEA"""
+        user_answer = input("Respuesta: ")
+        if user_answer.isdigit():
+            user_answer = int(user_answer) - 1
+        else:
+            print("Respuesta no Valida")
+            sys.exit(1)                 
         # Se verifica si la respuesta es correcta
+
+        """MODIFICO EL IF PARA VERIFICAR QUE LA RESPUESTA DADA ESTE ENTRE LAS OPCIONES POSIBLES"""
         if user_answer == correct_answers_index[question_index]:
             print("¡Correcto!")
             break
+        elif user_answer >= 0:
+            if user_answer < 4:
+                print("Incorrecto")
+                continue
+            else:
+                print("Respuesta no Valida")
+                sys.exit(1)       
+        else:
+            print("Respuesta no Valida")
+            sys.exit(1)
     else:
+
         # Si el usuario no responde correctamente después de 2 intentos, se muestra la respuesta correcta
+
         print("Incorrecto. La respuesta correcta es:")
         print(answers[question_index][correct_answers_index[question_index]])
+
     # Se imprime un blanco al final de la pregunta
+
     print()
