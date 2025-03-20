7f7611f442dc951b485f2f62c022aded38291488
Tomás Stavale <tomstavale@gmail.com>
2025-03-20 17:31:58 -0300

    PASO3

--- questions.py
+++ questions.py
@@ -33,16 +33,17 @@ correct_answers_index = [1, 2, 0, 3, 1]
 """CREO UNA VARIABLE PARA GUARDAR LOS PUNTOS DEL PLAYER"""
 PlayerPoints = 0
 
+"""TOME EL CODIGO DE AYUDA Y LO IMPLEMENTE"""
+questions_to_ask = random.choices(list(zip(questions,answers, correct_answers_index)), k=3)
+
 # El usuario deberá contestar 3 preguntas
 
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-
-    question_index = random.randint(0, len(questions)-1)
-
+for quest, solution, correct_solution in questions_to_ask:
+    
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+
+    print(quest)
+    for i, answer in enumerate(solution):
         print(f"{i + 1}. {answer}")
 
     # El usuario tiene 2 intentos para responder correctamente
@@ -57,16 +58,12 @@ for _ in range(3):
 
         # Se verifica si la respuesta es correcta
 
-        if user_answer == correct_answers_index[question_index]:
-
-            """SUMO +1p SI LA RESPUESTA FUE CORRECTA"""
+        if user_answer == correct_solution:
             PlayerPoints += 1
             print("¡Correcto! +1p")
             break
         elif user_answer >= 0:
             if user_answer < 4:
-
-                """RESTO -0.5p SI LA RESPUESTA FUE ERRONEA"""
                 PlayerPoints -= 0.5
                 print("Incorrecto -0.5p")
                 continue
@@ -81,7 +78,7 @@ for _ in range(3):
         # Si el usuario no responde correctamente después de 2 intentos, se muestra la respuesta correcta
 
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(solution[correct_solution])
 
     # Se imprime un blanco al final de la pregunta
 
